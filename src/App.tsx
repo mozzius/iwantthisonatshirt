719c79bb725d045fc4f63109a82c8bbe56054cf6
Samuel Newman <mozzius@protonmail.com>
2023-08-08 22:44:51 +0100

    all the code im not touching this again

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import model from "./assets/tshirt.png";
+
+const design = decodeURIComponent(
+  new URLSearchParams(window.location.search).get("design") ?? "default"
+);
+
+function App() {
+
+
+  return (
+    <main className="container mx-auto">
+      <h1 className="text-center -rotate-1 font-bold italic text-red-500 text-bold text-6xl animate-pulse">
+        MOZZIUSES 100% LEGIC TSHIRST STORE
+      </h1>
+      <div className="w-full flex">
+        <div className="relative flex-1">
+          <img src={model} alt="" className="w-full aspect-square" />
+          <img
+            src={design}
+            alt=""
+            className="absolute top-1/4 left-1/4 w-1/2"
+          />
+        </div>
+        <div className="flex-1 relative flex sm:flex-col">
+          <div className="animate-spin bg-red-500 absolute right-0 top-0 text-white w-24 h-24 rounded-lg">
+            99% off
+          </div>
+          <h2 className="text-red-500 text-4xl rotate-6 italic font-bold">ORIGINAL DESGIN TEESJERT</h2>
+          <ul className="relative left-4 -rotate-3 font-serif list-disc">
+            <li>100% pure asbestos blend</li>
+            <li>stuffed through a 2008 off brand inkjet printer</li>
+            <li>hand woven by children in the third world</li>
+          </ul>
+          <p className="mt-6 font-bold text-gray-700 rotate-[0.5deg] text-3xl">ONLY <span className="animate-ping text-blue-700">1</span> LEFT U BETTER HURRY</p>
+          <a
+            className="mt-10 relative font-bold text-2xl right-2 text-white animate-pulse bg-red-500 p-4 rounded-lg text-center"
+            href="https://github.com/sponsors/mozzius"
+          >
+            BUY NOW ONLY $60
+          </a>
+          <div className="grow"></div>
+          <p className="font-serif break-all animate-bounce">
+            buynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynow
+          </p>
+          <p className="font-serif absolute break-all bottom-0 animate-spin -z-10 w-full">
+            buynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynowbuynow
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
