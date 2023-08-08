4b967526e3836d9f06d632c204831cdaab583f65
Samuel Newman <mozzius@protonmail.com>
2023-08-08 23:47:42 +0100

    format

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ const design = decodeURIComponent(
 );
 
 function App() {
-
-
   return (
     <main className="container mx-auto">
       <h1 className="text-center -rotate-1 font-bold italic text-red-500 text-bold text-6xl animate-pulse">
@@ -25,13 +23,18 @@ function App() {
           <div className="animate-spin bg-red-500 absolute right-0 top-0 text-white w-24 h-24 rounded-lg">
             99% off
           </div>
-          <h2 className="text-red-500 text-4xl rotate-6 italic font-bold">ORIGINAL DESGIN TEESJERT</h2>
+          <h2 className="text-red-500 text-4xl rotate-6 italic font-bold">
+            ORIGINAL DESGIN TEESJERT
+          </h2>
           <ul className="relative left-4 -rotate-3 font-serif list-disc">
             <li>100% pure asbestos blend</li>
             <li>stuffed through a 2008 off brand inkjet printer</li>
             <li>hand woven by children in the third world</li>
           </ul>
-          <p className="mt-6 font-bold text-gray-700 rotate-[0.5deg] text-3xl">ONLY <span className="animate-ping text-blue-700">1</span> LEFT U BETTER HURRY</p>
+          <p className="mt-6 font-bold text-gray-700 rotate-[0.5deg] text-3xl">
+            ONLY <span className="animate-ping text-blue-700">1</span> LEFT U
+            BETTER HURRY
+          </p>
           <a
             className="mt-10 relative font-bold text-2xl right-2 text-white animate-pulse bg-red-500 p-4 rounded-lg text-center"
             href="https://github.com/sponsors/mozzius"
